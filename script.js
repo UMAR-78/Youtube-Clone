6aef0f21728d566b7bd0bbfa6545dcdd54d670a4
Umar Jamil <99021340+UMAR-78@users.noreply.github.com>
2023-03-22 01:49:17 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var menuicon = document.querySelector(".menuicon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+menuicon.onclick = function()
+{
+    sidebar.classList.toggle("smallsidebar");
+    container.classList.toggle("large_container");
+}
